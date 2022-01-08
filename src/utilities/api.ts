a469158c2d2dcc0311c3bcb15e83da16cda6995f
Sakis Simos <t.simos@atc.gr>
2022-01-08 23:05:45 +0200

    features: token to header, connect to user methods

--- src/utilities/api.ts
+++ src/utilities/api.ts
@@ -7,12 +7,34 @@ import * as storage from '../utilities/asyncStorage';
 // export const appUrl = 'http://172.16.201.73:3000'; //work
 export const appUrl = 'http://192.168.1.2:3000'; // home
 
+const getOptions = async () => {
+  const token = await storage.getItem('token');
+  return {
+    headers: new Headers({
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }),
+  };
+};
+
 const postOptions = (data: any) => {
   return {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
+    body: JSON.stringify(data),
+  };
+};
+
+const patchOptions = async (data: any) => {
+  const token = await storage.getItem('token');
+  return {
+    method: 'PATCH',
+    headers: new Headers({
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }),
     body: JSON.stringify(data),
   };
 };
@@ -35,12 +57,39 @@ export const signOut = async () => {
   return Promise.all(promises);
 };
 
-export const getAllUsers = async (token: string) => {
-  return await fetch(`${baseUrl}/users/all?token=${token}`);
+export const getAllUsers = async () => {
+  return await fetch(`${baseUrl}/users/all`, await getOptions());
 };
 
-export const getChat = async (roomId: string, limit: Number, token: string) => {
+export const getOneUser = async (uid: string) => {
+  const res = await fetch(`${baseUrl}/users/user/${uid}`, await getOptions());
+  return await res.json();
+};
+
+export const getChat = async (roomId: string, limit: Number) => {
   return await fetch(
-    `${baseUrl}/chat/?roomId=${roomId}&limit=${limit}&token=${token}`,
+    `${baseUrl}/chat/?roomId=${roomId}&limit=${limit}`,
+    await getOptions(),
   );
+};
+
+export const connectRequest = async (
+  senderUid: string,
+  receiverUid: string,
+) => {
+  const res = await fetch(
+    `${baseUrl}/users/connection-request`,
+    await patchOptions({ senderUid, receiverUid }),
+  );
+
+  return await res.json();
+};
+
+export const acceptConnect = async (senderUid: string, receiverUid: string) => {
+  const res = await fetch(
+    `${baseUrl}/users/connect`,
+    await patchOptions({ senderUid, receiverUid }),
+  );
+
+  return await res.json();
 };
