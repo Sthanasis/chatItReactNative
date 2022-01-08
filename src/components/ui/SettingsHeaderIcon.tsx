79399407a247e11acdbdd22f94c5e0d92636468a
Sakis Simos <t.simos@atc.gr>
2022-01-08 18:46:32 +0200

    Add linter comment

--- src/components/ui/SettingsHeaderIcon.tsx
+++ src/components/ui/SettingsHeaderIcon.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { TouchableNativeFeedback, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -12,8 +13,11 @@ const SettingsHeaderIcon = ({ navigation, color }: Props) => {
   return (
     <TouchableNativeFeedback onPress={() => navigation.navigate('Settings')}>
       <View
-        style={{ height: '100%', paddingVertical: 10, paddingHorizontal: 5 }}
-      >
+        style={{
+          height: '100%',
+          paddingVertical: 10,
+          paddingHorizontal: 5,
+        }}>
         <Icon name="gear" color={color} size={26} />
       </View>
     </TouchableNativeFeedback>
