7cb538b19c946f039b512f148db12e57623a4eb5
Sakis Simos <t.simos@atc.gr>
2022-01-08 23:04:09 +0200

    cleanup

--- src/screens/Chats.tsx
+++ src/screens/Chats.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Text, View, TouchableNativeFeedback } from 'react-native';
 import { NavPropsHome } from '../AppTypes';
 import screenStyles from '../styles/ScreenStyles';
@@ -8,13 +8,13 @@ import { Colors } from '../utilities/colors';
 
 const Chats = ({ navigation, route }: NavPropsHome): JSX.Element => {
   const theme = useAppSelector((state) => state.settingsState.theme);
+
   return (
     <View
       style={{
         ...screenStyles.screen,
         backgroundColor: theme === 'dark' ? Colors.dark : Colors.light,
-      }}
-    >
+      }}>
       <View>
         <Text style={theme === 'dark' && screenStyles.text}>
           This is the Chats page
