3ca87918c8db157903a7846ca89d4a02f86a6dbb
Sakis Simos <t.simos@atc.gr>
2022-01-09 22:53:22 +0200

    feature: add image and font size

--- src/components/ui/UserCard.tsx
+++ src/components/ui/UserCard.tsx
@@ -1,40 +1,57 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
-import { User } from '../../appTypes';
+import { UserDBSchema } from '../../appTypes';
+import { appUrl } from '../../utilities/api';
 import { Colors } from '../../utilities/colors';
 
 interface Props {
-  user: User;
+  user: UserDBSchema;
   theme: 'dark' | 'light';
   onUserCardPress: () => void;
 }
 
+const FONT_SIZE = 18;
+
 const UserCard = ({ user, theme, onUserCardPress }: Props): JSX.Element => {
+  const color = theme === 'dark' ? Colors.light : Colors.dark;
+
   return (
     <TouchableOpacity onPress={onUserCardPress}>
-      <View style={styles(theme).card}>
+      <View style={{ ...styles.card }}>
         <View>
-          <Text>{user.firstname}</Text>
+          <Image
+            style={styles.image}
+            source={{
+              uri: `${appUrl}${user.imageUrl}?time=${new Date()}`,
+            }}
+          />
         </View>
-        <View>
-          <Text>{user.lastname}</Text>
-        </View>
+        <Text style={{ color, fontSize: FONT_SIZE }}>{user.firstname}</Text>
+        <Text> </Text>
+        <Text style={{ color, fontSize: FONT_SIZE }}>{user.lastname}</Text>
       </View>
     </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create((theme: 'dark' | 'light') => {
-  return {
-    card: {
-      backgroundColor: theme === 'dark' ? 'white' : Colors.light,
-      width: '90%',
-      marginBottom: 10,
-      padding: 10,
-      borderRadius: 5,
-    },
-  };
+const styles = StyleSheet.create({
+  card: {
+    width: '100%',
+    marginBottom: 10,
+    padding: 10,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
+    alignItems: 'center',
+  },
+  image: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+    marginLeft: 10,
+  },
 });
 
 export default UserCard;
