6b7fb441a36d144a14be2ec47f51397063a98fd5
Sthanasis <sthanasis1994@gmail.com>
2022-01-14 17:18:49 +0200

    Add style prop

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ViewStyle } from 'react-native';
 import { ActivityIndicator, SafeAreaView } from 'react-native';
 import { AppTheme } from '../../appTypes';
 import ScreenStyles from '../../styles/ScreenStyles';
@@ -6,14 +7,16 @@ import { Colors } from '../../utilities/colors';
 
 interface Props {
   theme: AppTheme;
+  loaderStyles?: ViewStyle;
 }
 
-const Loader = ({ theme }: Props): JSX.Element => {
+const Loader = ({ theme, loaderStyles }: Props): JSX.Element => {
   return (
     <SafeAreaView
       style={{
         ...ScreenStyles.screen,
         backgroundColor: theme === 'dark' ? Colors.dark : Colors.light,
+        ...loaderStyles,
       }}>
       <ActivityIndicator
         size="large"
