eb10021083c77410026f7ac29a8d79bcfcbeb0d5
Sthanasis <sthanasis1994@gmail.com>
2022-01-18 11:31:56 +0200

    Add robolo Image

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,67 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import React from 'react';
+import { Image, View } from 'react-native';
 
-import { NavPropsHome, UserDBSchema } from '../AppTypes';
+import { NavPropsHome } from '../AppTypes';
 import screenStyles from '../styles/ScreenStyles';
 
-import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { useAppSelector } from '../store/hooks';
 import { Colors } from '../utilities/colors';
-import { getActiveConnections } from '../utilities/api';
-
-import UserCard from '../components/ui/UserCard';
-
-import Loader from '../components/ui/Loader';
-import { setError } from '../store/reducers/appSlice';
 
 const Home = ({ navigation }: NavPropsHome): JSX.Element => {
   const theme = useAppSelector((state) => state.settingsState.theme);
-  const [users, setUsers] = useState<UserDBSchema[]>([]);
-  const [loading, setLoading] = useState(false);
-  const loggedUser = useAppSelector((state) => state.userState.user);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        setLoading(true);
-        const response = await getActiveConnections(
-          loggedUser?.connectedTo as string[],
-        );
-        const res = await response.json();
-        setUsers(res.users);
-        setLoading(false);
-      } catch (err: any) {
-        dispatch(setError(`${err}`));
-        setLoading(false);
-      }
-    };
-
-    getData();
-  }, []);
-
-  if (loading) {
-    return <Loader theme={theme} />;
-  }
-  const handlePress = (user: UserDBSchema) => {
-    navigation.navigate('User', { user });
-  };
 
   return (
     <View
       style={{
-        ...screenStyles.screenTop,
+        ...screenStyles.screen,
         backgroundColor: theme === 'dark' ? Colors.dark : Colors.light,
       }}>
-      <ScrollView>
-        {users.map((user) => (
-          <UserCard
-            user={user}
-            key={user.uid}
-            theme={theme}
-            onUserCardPress={() => handlePress(user)}
-          />
-        ))}
-      </ScrollView>
+      <View>
+        <Image
+          source={require('../assets/img/robolo.png')}
+          style={{
+            tintColor: theme === 'dark' ? Colors.lighter : Colors.darker,
+          }}
+        />
+      </View>
     </View>
   );
 };
