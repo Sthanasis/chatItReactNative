78504df96b4129ccf519d96fb6b9822fc8651c34
Sthanasis <sthanasis1994@gmail.com>
2022-01-18 11:31:43 +0200

    Fetch profile and navigate on Chat screen on profile press

--- src/screens/Chats.tsx
+++ src/screens/Chats.tsx
@@ -1,25 +1,66 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Text, View, TouchableNativeFeedback } from 'react-native';
-import { NavPropsHome } from '../AppTypes';
+import { View, ScrollView } from 'react-native';
+import { NavPropsHome, UserDBSchema } from '../AppTypes';
 import screenStyles from '../styles/ScreenStyles';
-import buttonStyles from '../styles/ButtonStyles';
-import { useAppSelector } from '../store/hooks';
+
+import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { Colors } from '../utilities/colors';
+import { getActiveConnections } from '../utilities/api';
+import { setError } from '../store/reducers/appSlice';
+import Loader from '../components/ui/Loader';
+import UserCard from '../components/ui/UserCard';
 
 const Chats = ({ navigation, route }: NavPropsHome): JSX.Element => {
   const theme = useAppSelector((state) => state.settingsState.theme);
 
+  const [users, setUsers] = useState<UserDBSchema[]>([]);
+  const [loading, setLoading] = useState(false);
+  const loggedUser = useAppSelector((state) => state.userState.user);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const response = await getActiveConnections(
+          loggedUser?.connectedTo as string[],
+        );
+        const res = await response.json();
+        setUsers(res.users);
+        setLoading(false);
+      } catch (err: any) {
+        dispatch(setError(`${err}`));
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  const handlePress = (user: UserDBSchema) => {
+    navigation.navigate('ChatRoom', { user });
+  };
+
+  if (loading) {
+    return <Loader theme={theme} />;
+  }
   return (
     <View
       style={{
-        ...screenStyles.screen,
+        ...screenStyles.screenTop,
         backgroundColor: theme === 'dark' ? Colors.dark : Colors.light,
       }}>
-      <View>
-        <Text style={theme === 'dark' && screenStyles.text}>
-          This is the Chats page
-        </Text>
-      </View>
+      <ScrollView>
+        {users.map((user) => (
+          <UserCard
+            user={user}
+            key={user.uid}
+            theme={theme}
+            onUserCardPress={() => handlePress(user)}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 };
